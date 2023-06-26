e96974112d766b93d39f0f6768b0aad500f20892
Pranav Thakkar <113768273+Pranav1002@users.noreply.github.com>
2023-06-26 19:54:49 +0530

    Add files via upload

---
+++ Mock_Test_1/que2.py
@@ -0,0 +1,9 @@
+
+def firstUniqChar(s):
+    freq = {}
+    for c in s:
+        freq[c] = freq.get(c, 0) + 1
+    for i in range(len(s)):
+        if freq[s[i]] == 1:
+            return i
+    return -1
